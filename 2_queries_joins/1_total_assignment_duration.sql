c3a72847a5d3c553285ee6236306839a6d433ee8
maddie <lee.maddie@gmail.com>
2023-11-01 21:28:45 -0400

    Fix value name

--- 2_queries_joins/1_total_assignment_duration.sql
+++ 2_queries_joins/1_total_assignment_duration.sql
@@ -1,4 +1,4 @@
-SELECT SUM(duration) AS total_duration
+SELECT SUM(assignment_submissions.duration) AS total_duration
 FROM assignment_submissions
 JOIN students ON students.id = student_id
 WHERE students.name = 'Ibrahim Schimmel';
