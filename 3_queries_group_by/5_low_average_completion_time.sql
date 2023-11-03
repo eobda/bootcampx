ab5e0ecadf947037804c70561f0337ad5e341a50
maddie <lee.maddie@gmail.com>
2023-11-02 22:35:17 -0400

    Fix syntax

--- 3_queries_group_by/5_low_average_completion_time.sql
+++ 3_queries_group_by/5_low_average_completion_time.sql
@@ -4,4 +4,5 @@ JOIN assignment_submissions ON student_id = students.id
 JOIN assignments ON assignment_id = assignments.id
 WHERE end_date IS NULL
 GROUP BY student
+HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
 ORDER BY average_assignment_duration;
