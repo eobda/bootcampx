55eac1c6aa75629cb3204d7f02921583e2f9150b
maddie <lee.maddie@gmail.com>
2023-11-03 09:36:59 -0400

    Create assistance_requests table

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -5,3 +5,15 @@ CREATE TABLE teachers (
   end_date DATE,
   is_active BOOLEAN
 );
+
+CREATE TABLE assistance_requests (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+  created_at DATETIME,
+  started_at DATETIME,
+  completed_at DATETIME,
+  student_feedback TEXT,
+  teacher_feedback TEXT
+);
