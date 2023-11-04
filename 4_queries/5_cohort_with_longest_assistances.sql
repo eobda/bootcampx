645967d4aec5f08b85a22654949aa0527a8e18ba
maddie <lee.maddie@gmail.com>
2023-11-04 15:15:54 -0400

    Use LIMIT

--- 4_queries/5_cohort_with_longest_assistances.sql
+++ 4_queries/5_cohort_with_longest_assistances.sql
@@ -3,5 +3,5 @@ FROM assistance_requests
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = cohort_id
 GROUP BY cohorts.name
-HAVING AVG(completed_at-started_at) = MAX(completed_at-started_at)
-ORDER BY average_assistance_time;
+ORDER BY average_assistance_time DESC
+LIMIT 1;
