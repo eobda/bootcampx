91fb03ffa88a43357fa269ed24087d5ad8e94ba1
maddie <lee.maddie@gmail.com>
2023-11-04 17:19:04 -0400

    Fix syntax

--- 4_queries/12_teachers_and_assistances.sql
+++ 4_queries/12_teachers_and_assistances.sql
@@ -4,4 +4,5 @@ JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
 ORDER BY teacher;
