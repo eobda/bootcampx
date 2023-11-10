52d00319a7e37305885b6bfeec65941103408ae5
maddie <lee.maddie@gmail.com>
2023-11-10 09:20:51 -0500

    Lint code

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -20,9 +20,9 @@ const limit = process.argv[3] || 5;
 const values = [`%${cohortName}%`, limit];
 
 pool.query(queryString, values)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
   })
-})
-.catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack));
