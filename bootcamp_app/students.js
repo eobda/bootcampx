0f4a11614e6bc8cd6da0cd33ad0204770e1b595b
maddie <lee.maddie@gmail.com>
2023-11-09 22:28:35 -0500

    Update console log result

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,12 +8,14 @@ const pool = new Pool({
 });
 
 pool.query(`
-  SELECT students.id AS id, students.name AS name, cohorts.name AS cohort
+  SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
   FROM students
   JOIN cohorts ON cohorts.id = cohort_id
   LIMIT 5;
   `)
 .then(res => {
-  console.log(res.rows);
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
 })
 .catch(err => console.error('query error', err.stack));
